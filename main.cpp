13504c25ea2deefdcfe4526ea67c53647e6e3702
전태경 <xorud1544@naver.com>
2020-05-18 21:42:59 +0900

    타이머 콜백 등록하기 Q) 왜 중간에 사라지지?

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <gl/GL.h>
 #include <gl/GLU.h>
 
+GLfloat delta = 0.0;
 void MyDisplay()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
@@ -9,15 +10,22 @@ void MyDisplay()
 	glutSolidSphere(0.5, 100, 100);
 	glEnd();
 
+	glPushMatrix();
 	glColor3f(0.0, 0.0, 255.0);
+	glRotatef((GLfloat)delta, 0.0, 1.0, 0.0);
 	glTranslatef(-3.0, 0.0, 0.0);
 	glutSolidSphere(0.25, 100, 100);
 	glEnd();
 
+	glPushMatrix();
 	glColor3f(128.0, 128.0, 128.0);
+	glRotatef((GLfloat)delta, 0.0, 1.0, 0.0);
 	glTranslatef(-0.5, 0.0, 0.0);
 	glutSolidSphere(0.05, 100, 100);
 	glEnd();
+
+	glPopMatrix();
+	glPopMatrix();
 	glFlush();
 }
 
@@ -32,12 +40,20 @@ void MyReshape(int width, int height)
 	glOrtho(-1.0 * widthFactor, 1.0 * widthFactor, -1.0 * heightFactor, 1.0 * heightFactor, -1.0, 1.0);
 }
 
+void MyTimer(int time)
+{
+	delta = delta + 0.1;
+	glutPostRedisplay();
+	glutTimerFunc(10, MyTimer, 1);
+}
+
 int main()
 {
 	glutInitWindowSize(1500, 700);
 	glutCreateWindow("OpenGL Solar System");
 	glutDisplayFunc(MyDisplay);
 	glutReshapeFunc(MyReshape);
+	glutTimerFunc(100, MyTimer, 1);
 	glutMainLoop();
 	return 0;
 }
